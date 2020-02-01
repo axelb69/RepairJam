afa01ec097d53a957c9ce5e1ae3d82d78fdf009b
axelb69 <50579454+axelb69@users.noreply.github.com>
2020-02-01 11:08:18 +0100

    kalashestchargée

--- Assets/Axel/script/Buldings/Repair.cs
+++ Assets/Axel/script/Buldings/Repair.cs
@@ -7,7 +7,8 @@ public class Repair : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log(TerrainManager.Instance.builds);
+        TerrainManager.Instance.builds.Add(transform);
     }
 
     // Update is called once per frame
--- Assets/Axel/script/charactere/Controler.cs
+++ Assets/Axel/script/charactere/Controler.cs
@@ -4,12 +4,22 @@ using UnityEngine;
 
 public class Controler : MonoBehaviour
 {
-    [SerializeField] private float _speed;
+    [SerializeField] private float _speed = 1;
+    [SerializeField] private Rigidbody _rb = null;
     void Update()
     {
         Vector3 movePos = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         movePos = Vector3.Normalize(movePos);
         movePos *= _speed * Time.deltaTime;
-        transform.Translate(movePos);
+        _rb.MovePosition(transform.position + movePos);
+    }
+    private void OnTriggerStay(Collider other)
+    {
+        if(Input.GetKeyDown(KeyCode.E) && other.gameObject.layer == 8)
+        {
+            InventoryManager.Instance.slots[1] += 1;
+            //Debug.Log(InventoryManager.Instance.slots[1]);
+            TerrainManager.Instance.builds[TerrainManager.Instance.builds.IndexOf(other.gameObject.transform)].transform.position +=  Vector3.up;
+        }
     }
 }
