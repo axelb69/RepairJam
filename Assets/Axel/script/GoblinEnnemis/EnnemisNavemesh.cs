d9fe414680b45963a98704e4f366b9bdc1b91c9c
axelb69 <50579454+axelb69@users.noreply.github.com>
2020-02-01 16:25:08 +0100

    git

--- Assets/Axel/script/GoblinEnnemis/EnnemisNavemesh.cs
+++ Assets/Axel/script/GoblinEnnemis/EnnemisNavemesh.cs
@@ -6,17 +6,43 @@ using UnityEngine.AI;
 public class EnnemisNavemesh : MonoBehaviour
 {
     private NavMeshAgent _angent;
-    private Transform _destination;
+    private Vector3 _destination = Vector3.zero;
+    private float _wait;
     // Start is called before the first frame update
     void Start()
     {
+        _wait = Time.time;
         _angent = GetComponent<NavMeshAgent>();
-        _angent.SetDestination(_destination.position);
+        float _size = TerrainManager.Instance.size;
+        foreach (Transform t in TerrainManager.Instance.builds)
+        {
+            Repair build = t.GetComponent<Repair>();
+            if (!build.caseTake[0])
+            {
+                build.caseTake[0] = true;
+                _destination = t.position + Vector3.left * _size;
+            }
+            else if (!build.caseTake[1])
+            {
+                build.caseTake[1] = true;
+                _destination = t.position + Vector3.back * _size;
+            }
+            else if (!build.caseTake[2])
+            {
+                build.caseTake[2] = true;
+                _destination = t.position + Vector3.right * _size;
+            }
+            if (_destination != Vector3.zero)
+            {
+                break;
+            }
+        }
+        _angent.SetDestination(_destination);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
 }
---
+++ Assets/Julie/Waves/WavesManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Engine.Utils;
+using UnityEngine;
+
+public class WavesManager : Singleton<WavesManager>
+{
+   
+}
