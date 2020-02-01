6df0227e4077a90e9b0fe8e821a25b988ac0a2b2
axelb69 <50579454+axelb69@users.noreply.github.com>
2020-02-01 05:10:41 +0100

    goo

---
+++ Assets/Axel/script/GoblinEnnemis/EnnemisNavemesh.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnnemisNavemesh : MonoBehaviour
+{
+    private NavMeshAgent _angent;
+    [SerializeField] private Transform _destination;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _angent = GetComponent<NavMeshAgent>();
+        _angent.SetDestination(_destination.position);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
---
+++ Assets/Axel/script/charactere/Controler.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controler : MonoBehaviour
+{
+    [SerializeField] private float _speed;
+    void Update()
+    {
+        Vector3 movePos = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        movePos = Vector3.Normalize(movePos);
+        movePos *= _speed * Time.deltaTime;
+        transform.Translate(movePos);
+    }
+}
