9c134b16a22eb51a0305e96afeab8e636d4b2ae6
axelb69 <50579454+axelb69@users.noreply.github.com>
2020-02-01 13:26:52 +0100

    git

---
+++ Assets/Axel/script/GoblinEnnemis/EnnemisSpawn.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnnemisSpawn : MonoBehaviour
+{
+    [SerializeField] private float _timeBetweenSpawn = 1;
+    [SerializeField] private GameObject _ennemisObject = null;
+
+    private float _reachTime = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _reachTime = Time.time + _timeBetweenSpawn;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time >= _reachTime && _ennemisObject != null)
+        {
+            _reachTime = Time.time + _timeBetweenSpawn;
+            Instantiate(_ennemisObject, transform.position, Quaternion.identity, transform);
+        }
+    }
+}
