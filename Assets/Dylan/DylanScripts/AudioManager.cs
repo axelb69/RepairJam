bc9840b1efc51868aa6bc285353ea10d75c5f49f
Dylan <brousse.dylan@gmail.com>
2020-02-02 06:06:53 +0100

    lalalaalalalalal

---
+++ Assets/Dylan/DylanScripts/AudioManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Engine.Utils;
+
+public class AudioManager : Singleton<AudioManager>
+{
+    private AudioSource _audioSouce;
+    public AudioSource audioSouce{get{return _audioSouce;}}
+
+    [SerializeField]
+    private AudioClip[] _clips;
+    public AudioClip[] clips {get{return _clips;}}
+}
