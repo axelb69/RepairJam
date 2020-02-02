b6c5612f92592f478ef30152a09829c818047388
daquxxn <50579443+daquxxn@users.noreply.github.com>
2020-02-02 13:43:34 +0100

    n

---
+++ Assets/Julie/ScriptsJulie/Wave_count.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class Wave_count : MonoBehaviour
+{
+
+    public int _waveCount = 0;
+    [SerializeField] private TextMeshProUGUI _printWave;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _printWave.text = WavesManager.Instance.wave.ToString();
+    }
+}
