b02e7a1a3b75664e6a99e64f47b3292edf334418
axelb69 <50579454+axelb69@users.noreply.github.com>
2020-02-02 12:53:12 +0100

    go

--- Assets/Dylan/DylanScripts/LootRessources.cs
+++ Assets/Dylan/DylanScripts/LootRessources.cs
@@ -4,26 +4,28 @@ using UnityEngine;
 
 public class LootRessources : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     void OnTriggerEnter(Collider col)
     {
-        AudioManager.Instance.audioSouce.clip = AudioManager.Instance.clips[9];
-        AudioManager.Instance.audioSouce.Play();
-        //InventoryManager.Instance.slots[0] += col.GetComponent<RessourcesInformations>().ressourcesInformations[0];
-        //InventoryManager.Instance.slots[1] += col.GetComponent<RessourcesInformations>().ressourcesInformations[1];
-        //InventoryManager.Instance.slots[2] += col.GetComponent<RessourcesInformations>().ressourcesInformations[2];
-        
-        Destroy(col.gameObject, 0.1f);
+        if (col.gameObject.layer == 9)
+        {
+            AudioManager.Instance.audioSouce.clip = AudioManager.Instance.clips[9];
+            AudioManager.Instance.audioSouce.Play();
+            if (col.gameObject.GetComponent<bone>() != null)
+            {
+                InventoryManager.Instance.slots[0] += col.GetComponent<bone>().cunt;
+            }
+            else if (col.gameObject.GetComponent<stone>() != null)
+            {
+                InventoryManager.Instance.slots[1] += col.GetComponent<stone>().cunt;
+            }
+            else if (col.gameObject.GetComponent<Wood>() != null)
+            {
+                InventoryManager.Instance.slots[2] += col.GetComponent<Wood>().cunt;
+            }
+            Destroy(col.gameObject, 0.1f);
+        }
+       
     }
 }
